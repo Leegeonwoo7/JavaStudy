68639df11ad13a0fd69a8b0b9b58fec03ff58b39
Leegeonwoo7 <workgeon07@naver.com>
2023-12-05 23:54:38 +0900

    큐 구현

---
+++ src/ch37/MyListQueue.java
@@ -0,0 +1,58 @@
+package ch37;
+
+import ch35.MyLinkedList;
+import ch35.MyListNode;
+
+interface IQueue{
+	public void enQueue(String data);
+	public String deQueue();
+	public void printAll();
+}
+
+public class MyListQueue extends MyLinkedList implements IQueue{
+	MyListNode front;
+	MyListNode rear;
+	
+	public MyListQueue() {
+		front = null;
+		rear = null;
+	}
+	
+	public void enQueue(String data) {
+		MyListNode newNode;
+		if(isEmpty()) {
+			newNode = addElement(data);
+			front = newNode;
+			rear = newNode;
+		}else {
+			newNode = addElement(data);
+			rear = newNode;
+		}
+		System.out.println(newNode.getData() + " added");
+	}
+	
+	public String deQueue() {
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return null;
+		}
+		String data = front.getData();
+		front = front.next;
+		if(front == null) {
+			rear = null;
+		}return data;
+	}
+	
+	public void printAll() {
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		MyListNode temp = front;
+		while(temp!=null) {
+			System.out.print(temp.getData() + ",");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+}
