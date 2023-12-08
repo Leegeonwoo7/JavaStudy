c65b022d6ba02663302763d64bf8727b6935993d
Leegeonwoo7 <workgeon07@naver.com>
2023-12-09 06:53:59 +0900

    실행 클래스

--- src/array_apply_project/Application.java
+++ src/array_apply_project/Application.java
@@ -4,33 +4,12 @@ import java.util.Scanner;
 
 public class Application {
 	public static void main(String[] args) {
-		Student[] studentList;
-		int studentCount;
-		
 		Scanner sc = new Scanner(System.in);
-		studentCount = sc.nextInt();
-		studentList = new Student[studentCount];
+		System.out.print("학생수를 입력하세요: ");
+		int studentCount = sc.nextInt();
 		
-		for(int i=0; i<studentCount; i++) {
-			Student student = new Student();
-			student.setInfo();
-			studentList[i] = student;
-		}
-		int total = 0;
-		double avg = 0;
-		
-		for(int i=0; i<studentCount; i++) {
-			total += studentList[i].getScore();
-		}
-		avg = total / studentCount;
-		
-		System.out.println("평균 이상인 학생들: ");
-		for(int i=0; i<studentCount; i++) {
-			if(studentList[i].getScore() >= avg) {
-				System.out.print(studentList[i].getStudentName() + " - " + studentList[i].getScore() );
-				System.out.println();
-			}
-		}
+		StudentProcessor studentProcessor = new StudentProcessor(studentCount);
+		studentProcessor.run();
+		sc.close();
 	}
-	
 }
