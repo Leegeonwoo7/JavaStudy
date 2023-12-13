244a71f569bbe356da51eeed2a69ba8c72df8ec7
Leegeonwoo7 <workgeon07@naver.com>
2023-12-14 07:23:10 +0900

    InnerClass apply

---
+++ src/innerClass_Instance_apply/InnerTest.java
@@ -0,0 +1,35 @@
+package innerClass_Instance_apply;
+
+class OutClass {
+	
+	private int num = 10;
+	private static int sNum = 20;
+	private InClass inClass;
+	
+	public OutClass() {
+		inClass = new InClass();
+	}
+	
+	private class InClass{
+		int inNum = 100;
+		//static int sInNum = 200; //에러
+		
+		void inTest() {
+			System.out.println("OutClass num = " + num + "(외부 클래스의 인스턴스 변수)");
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 스태틱 변수)");
+			System.out.println("InClass inNum = " + inNum + "(내부 클래스의 인스턴스 변수)");
+		}
+	}
+	public void usingClass() {
+		inClass.inTest();
+	}	
+}
+
+public class InnerTest {
+	public static void main(String[] args) {
+		OutClass outClass = new OutClass();
+		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
+		outClass.usingClass();
+		System.out.println();
+	}
+}
